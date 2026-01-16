cde56f65a51ef44298bac7539711ff47f14b355e
guicoelhodev <gs.coelho_dev@outlook.com>
2026-01-16 19:13:49 -0300

    fix: fix colors from sun and background

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -2,11 +2,12 @@
 	import { T, useThrelte } from '@threlte/core'
 	import Player from './Player.svelte'
 	import Terrain from './Terrain.svelte'
+	import Sun from './Sun.svelte'
 	import { createNoise2D } from 'simplex-noise'
 	import { Color } from 'three'
 
 	const { scene } = useThrelte()
-	scene.background = new Color('#87CEEB')
+	scene.background = new Color('#84BEE3')
 
 	const noise2D = createNoise2D()
 	const maxHeight = 6
@@ -22,8 +23,8 @@
 </script>
 
 <T.HemisphereLight args={['#ffffff', '#444444', 0.6]} />
-<T.DirectionalLight position={[50, 100, 25]} intensity={0.8} />
 <T.AmbientLight intensity={0.3} />
 
 <Player bind:positionX={playerX} bind:positionZ={playerZ} {getHeight} />
 <Terrain playerX={playerX} playerZ={playerZ} {getHeight} />
+<Sun playerX={playerX} playerZ={playerZ} />
---
+++ src/lib/components/Sun.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { T } from '@threlte/core'
+	import { SphereGeometry, MeshBasicMaterial } from 'three'
+
+	interface Props {
+		playerX: number
+		playerZ: number
+	}
+
+	let { playerX, playerZ }: Props = $props()
+
+	const sunDistance = 200
+	const sunHeight = 150
+	const sunSize = 15
+
+	const geometry = new SphereGeometry(sunSize, 32, 32)
+	const material = new MeshBasicMaterial({
+		color: '#FFEA87'
+	})
+
+	// Posição do sol relativa ao player
+	$effect(() => {
+		sunX = playerX + sunDistance
+		sunZ = playerZ - sunDistance
+	})
+
+	let sunX = $state(sunDistance)
+	let sunZ = $state(-sunDistance)
+</script>
+
+<T.Mesh {geometry} {material} position.x={sunX} position.y={sunHeight} position.z={sunZ} />
+
+
